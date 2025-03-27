a081a18bdb383ef499d1bd841582ca14c2bba7dd
frozenoffical <tenopno@gmail.com>
2025-03-27 20:16:32 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,9 +73,9 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
-        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_spy_pro")
-        premium = InlineKeyboardButton("Get Premium", url="https://t.me/kingofpatal")   
+        image_url = "https://files.catbox.moe/x1wije.jpeg"
+        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/vibeshiftbots")
+        premium = InlineKeyboardButton("Get Premium", url="https://t.me/xyz09723")   
         keyboard = InlineKeyboardMarkup([
             [join_button],   
             [premium]    
