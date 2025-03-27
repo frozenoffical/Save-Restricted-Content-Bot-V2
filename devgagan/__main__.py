821b2d3883a7e503ea8e5d4b4d9943130e047b56
frozenoffical <tenopno@gmail.com>
2025-03-27 19:59:01 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -41,12 +41,12 @@ async def devggn_boot():
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
 👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
-🗓️ Created: 2025-01-11
+🌐 GitHub: https://github.com/prashantsahlot
+📬 Telegram: https://t.me/frozensupport1
+▶️ YouTube: https://www.youtube.com/@VibeShiftGamer
+🗓️ Created: 2022-01-11
 🔄 Last Modified: 2025-01-11
-🛠️ Version: 2.0.5
+🛠️ Version: 7.1.5
 📜 License: MIT License
 ---------------------------------------------------
 """)
