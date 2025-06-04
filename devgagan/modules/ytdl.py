f53d66e6d2f122ef79639fb87d8ea4df4335f996
frozenoffical <tenopno@gmail.com>
2025-06-04 14:37:16 +0530

    Update ytdl.py

--- devgagan/modules/ytdl.py
+++ devgagan/modules/ytdl.py
@@ -90,7 +90,7 @@ async def process_audio(client, event, url, cookies_env_var=None):
             temp_cookie_path = temp_cookie_file.name
  
     start_time = time.time()
-    random_filename = f"@team_spy_pro_{event.sender_id}"
+    random_filename = f"@vibeshiftbots{event.sender_id}"
     download_path = f"{random_filename}.mp3"
  
     ydl_opts = {
@@ -121,8 +121,8 @@ async def process_audio(client, event, url, cookies_env_var=None):
                 except Exception:
                     pass
                 audio_file.tags["TIT2"] = TIT2(encoding=3, text=title)
-                audio_file.tags["TPE1"] = TPE1(encoding=3, text="Team SPY")
-                audio_file.tags["COMM"] = COMM(encoding=3, lang="eng", desc="Comment", text="Processed by Team SPY")
+                audio_file.tags["TPE1"] = TPE1(encoding=3, text="@vibeshiftbots")
+                audio_file.tags["COMM"] = COMM(encoding=3, lang="eng", desc="Comment", text="Processed by @vibeshiftbots")
  
                 thumbnail_url = info_dict.get('thumbnail')
                 if thumbnail_url:
@@ -150,7 +150,7 @@ async def process_audio(client, event, url, cookies_env_var=None):
                 name=None,
                 progress_bar_function=lambda done, total: progress_callback(done, total, chat_id)
             )
-            await client.send_file(chat_id, uploaded, caption=f"**{title}**\n\n**__Powered by Team SPY__**")
+            await client.send_file(chat_id, uploaded, caption=f"**{title}**\n\n**__Powered by @vibeshiftbots**")
             if prog:
                 await prog.delete()
         else:
@@ -355,7 +355,7 @@ async def process_video(client, event, url, cookies_env_var, check_duration_and_
             return
          
         await asyncio.to_thread(download_video, url, ydl_opts)
-        title = info_dict.get('title', 'Powered by Team SPY')
+        title = info_dict.get('title', 'Powered by @vibeshiftbots')
         k = video_metadata(download_path)      
         W = k['width']
         H = k['height']
